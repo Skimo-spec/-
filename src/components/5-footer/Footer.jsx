f6fc6667263ab9f5d91d93d34433191c1dd43370
Hassan Ahmed <hassanahmed9887@gmail.com>
2025-08-15 06:28:55 +0300

    Update Footer.jsx

--- src/components/5-footer/Footer.jsx
+++ src/components/5-footer/Footer.jsx
@@ -3,12 +3,12 @@ import  './footer.css';
 
 const Footer = () => {
     return (
-        <footer className='flex'>
+        <footer className='flex' id="users">
             <ul className='flex'>
-                <li><a href="">About</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">Contact</a></li>
-                <li><a href="">Users</a></li>
+                <li><a href="#Home">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#users">Users</a></li>
             </ul>
 
             <p>&copy; 2025 <span>Skimo</span> All Rights Reserved.</p>
